852a7b6ea4bdab5c5c06253cb40db744f2a46851
HOME-PC\Andrii <panasiukandriy87@gmail.com>
2021-12-02 17:01:35 +0300

    finish lesson34 EventListeners

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,8 +1,12 @@
 export class DomListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error('no $root provided from DomListener');
     }
     this.$root = $root;
+    this.listeners = listeners;
   }
+
+  initDOMListeners() {}
+  removeDOMListeners() {}
 }
