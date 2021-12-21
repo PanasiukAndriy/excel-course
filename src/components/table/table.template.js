7da2b4fea42421237a04a0a9e02968349930c407
HOME-PC\Andrii <panasiukandriy87@gmail.com>
2021-12-21 17:43:31 +0300

    finish lesson 38. add cells

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,9 +3,9 @@ const CODES = {
   Z: 90,
 };
 
-function createCell() {
+function toCell() {
   return `
-        <div class="cell" contenteditable>B2</div>
+        <div class="cell" contenteditable></div>
     `;
 }
 
@@ -15,10 +15,10 @@ function toColumn(col) {
     `;
 }
 
-function createRow(content) {
+function createRow(index, content) {
   return `
   <div class="row">
-    <div class="row-info"></div>
+    <div class="row-info">${index ? index : ''}</div>
     <div class="row-data">${content}</div>
   </div>`;
 }
@@ -35,9 +35,10 @@ export function createTable(rowsCount = 15) {
   const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('');
 
   console.log(cols);
-  rows.push(createRow(cols));
+  rows.push(createRow(null, cols));
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow());
+    const cells = new Array(colsCount).fill('').map(toCell).join('');
+    rows.push(createRow(i + 1, cells));
   }
   return rows.join('');
 }
