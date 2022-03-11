e835064670cd6666cbd29f2907bef48c3dea0606
DESKTOP-LHRMO7E\andri <panasiukandriy87@gmail.com>
2022-03-12 00:43:18 +0200

    finish lesson 42  -  add resizer

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,7 +3,16 @@ import {createTable} from './table.template';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
+
+  constructor($root){
+    super($root, {
+      //listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+    })
+  }
+
   toHTML() {
     return createTable();
   }
+
+  
 }
