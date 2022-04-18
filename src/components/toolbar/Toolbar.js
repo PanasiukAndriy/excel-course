0944a2d584db898ed075be8262752cdc5f5805be
DESKTOP-LHRMO7E\andri <panasiukandriy87@gmail.com>
2022-04-18 21:42:04 +0300

    finish lesson 6.11  ExcelStateComponent

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,8 +1,8 @@
-import {ExcelComponent} from '../../core/ExcelComponent';
 import { createToolbar } from './toolbar.template';
 import {$} from '../../core/dom';
+import { ExcelStateComponent } from '../../core/ExcelStateComponent';
 
-export class Toolbar extends ExcelComponent {
+export class Toolbar extends ExcelStateComponent{
   static className = 'excel__toolbar';
 
   constructor($root, options) {
@@ -12,14 +12,34 @@ export class Toolbar extends ExcelComponent {
       ...options
     });
   }
+prepare(){
+  const initialState ={
+    textAlign: 'left',
+    fontWeight: 'normal',
+    textDecoration: 'none',
+    fontStyle: 'normal'
+  }
+  this.initState(initialState);
+}
+
+get template(){
+  return createToolbar(this.state);
+}
+
   toHTML() {
-    return createToolbar();
+    return this.template;
   }
 
   onClick(event){
     const $target = $(event.target)
     if($target.data.type === 'button'){
-      console.log('onClick', $target.data.value);
+      const value = JSON.parse($target.data.value);
+      const key = Object.keys(value)[0];
+      console.log(key);
+      this.setState({[key]:value[key]})
+
+      console.log(this.state);  
+      
     }
     
   }
